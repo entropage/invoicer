e2bdb87511dccf65d88138a788d3e0742b2828e5
Geng Yang <gengyang@gmail.com>
2024-12-23 03:31:28 -0800

    fix: use Fusion.js plugins for middleware

--- src/main.js
+++ src/main.js
@@ -4,11 +4,34 @@ import App from 'fusion-react';
 import HelmetPlugin from 'fusion-plugin-react-helmet-async';
 import mongoose from 'mongoose';
 import Router from 'fusion-plugin-react-router';
+import {createPlugin} from 'fusion-core';
 
 // src
 import {App as ClientApp} from './App';
 import handlers from './handlers';
 import {MONGODB_URI} from './constants';
+
+// Create plugins
+const BodyParserPlugin = createPlugin({
+  middleware: () => async (ctx, next) => {
+    if (ctx.request.is('application/json')) {
+      let body = '';
+      for await (const chunk of ctx.req) {
+        body += chunk.toString();
+      }
+      try {
+        ctx.request.body = JSON.parse(body);
+      } catch (e) {
+        ctx.request.body = {};
+      }
+    }
+    return next();
+  }
+});
+
+const HandlersPlugin = createPlugin({
+  middleware: () => handlers
+});
 
 export default function() {
   const app = new App(ClientApp);
@@ -25,27 +48,8 @@ export default function() {
         console.log('Mongodb Connected.');
       });
 
-    app.middleware((ctx, next) => {
-      if (ctx.request.is('application/json')) {
-        return new Promise((resolve) => {
-          let body = '';
-          ctx.req.on('data', chunk => {
-            body += chunk.toString();
-          });
-          ctx.req.on('end', () => {
-            try {
-              ctx.request.body = JSON.parse(body);
-            } catch (e) {
-              ctx.request.body = {};
-            }
-            resolve(next());
-          });
-        });
-      }
-      return next();
-    }, 1);
-
-    app.middleware(handlers);
+    app.register(BodyParserPlugin);
+    app.register(HandlersPlugin);
   }
 
   return app;
