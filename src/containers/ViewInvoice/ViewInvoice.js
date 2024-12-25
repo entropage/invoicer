0e90b42b6f90484428b71fa1c79087cdd44fe372
wangchong <wangziyan92@outlook.com>
2024-12-25 18:32:55 +0800

    Fix issue with viewInvoice

--- src/containers/ViewInvoice/ViewInvoice.js
+++ src/containers/ViewInvoice/ViewInvoice.js
@@ -1,21 +1,21 @@
 // @flow
 // libs
-import React, {useEffect, useReducer} from 'react';
-import {Formik} from 'formik';
-import {RouteComponentProps} from 'react-router';
+import React, { useEffect, useReducer } from 'react';
+import { Formik } from 'formik';
+import { RouteComponentProps } from 'react-router';
 
 // src
-import {copyTextToClipboard, getBrowserOrigin} from '../../utils';
-import {downloadInvoice, fetchInvoiceById} from '../../actions';
-import {FormContainer} from '../FormContainer';
-import {reducer} from './reducer';
-import {useSnackbar} from '../../components/Snackbar/useSnackbar';
-import {Values} from '../../types';
+import { copyTextToClipboard, getBrowserOrigin } from '../../utils';
+import { downloadInvoice, fetchInvoiceById } from '../../actions';
+import { FormContainer } from '../FormContainer';
+import { reducer } from './reducer';
+import { useSnackbar } from '../../components/Snackbar/useSnackbar';
+import { Values } from '../../types';
 
 export function ViewInvoice(props: RouteComponentProps) {
-  const {id} = props.match.params;
-  const [{isLoading, payload}, dispatch] = useReducer(reducer, {});
-  const {showSnackbar} = useSnackbar();
+  const { id } = props.match.params;
+  const [{ isLoading, payload }, dispatch] = useReducer(reducer, {});
+  const { showSnackbar } = useSnackbar();
 
   useEffect(() => {
     fetchInvoiceById(id, dispatch);
@@ -28,19 +28,18 @@ export function ViewInvoice(props: RouteComponentProps) {
   if (payload) {
     return (
       <Formik
-        initialValues={payload}
+        initialValues={{
+          invoice: payload,
+          isEditable: false,
+        }}
         component={FormContainer}
-        onSubmit={({action, invoice}: Values) => {
+        onSubmit={({ action, invoice }: Values) => {
           switch (action) {
             case 'download': {
               return downloadInvoice(payload);
             }
             case 'share': {
-              return copyTextToClipboard(
-                `${getBrowserOrigin()}/${invoice.invoiceId}`
-              ).then(() =>
-                showSnackbar('Shareable link copied to clipboard.', 'success')
-              );
+              return copyTextToClipboard(`${getBrowserOrigin()}/${invoice.invoiceId}`).then(() => showSnackbar('Shareable link copied to clipboard.', 'success'));
             }
             case 'print': {
               return window.print();
